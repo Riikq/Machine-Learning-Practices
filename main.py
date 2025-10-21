cd8dc5a43b68f7567a2f1c7f6248c9f2e78fb09d
Riikq <tariqahmad0510@gmail.com>
2025-10-21 20:19:07 +0700

    missing values

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import pandas as pd
 
 test = pd.read_csv("test.csv")
-print(test.head())
+# print(test.head())
 
 train = pd.read_csv("train.csv")
-print(train.head())
+# print(train.head())
+
+# print(train.info())
+# print(train.describe(include="all"))
+
+missing_values = train.isnull().sum()
+# print(missing_values[missing_values > 0])
+
+less = missing_values[missing_values < 1000].index
+over = missing_values[missing_values >= 1000].index
+
+numeric_features = train[less].select_dtypes(include=["number"]).columns
+train[numeric_features] = train[numeric_features].fillna(
+    train[numeric_features].median()
+)
+
+kategorical_features = train[less].select_dtypes(include=["object"]).columns
+for column in kategorical_features:
+    train[column] = train[column].fillna(train[column].mode()[0])
+
+df = train.drop(columns=over)
+
+missing_values = df.isnull().sum()
+print(missing_values[missing_values > 0])
