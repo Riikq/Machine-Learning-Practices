cffa4f4d670626cff3655a1d2939a143344bff22
Riikq <tariqahmad0510@gmail.com>
2025-10-22 20:41:16 +0700

    outliers final

--- outliers.py
+++ outliers.py
@@ -9,3 +9,17 @@ for feature in mv.numeric_features:
     plt.title(f"Box Plot of {feature}")
     plt.savefig
     plt.show(block=True)
+
+Q1 = mv.df[mv.numeric_features].quantile(0.25)
+Q3 = mv.df[mv.numeric_features].quantile(0.75)
+IQR = Q3 - Q1
+
+condition = ~(
+    (mv.df[mv.numeric_features] < (Q1 - 1.5 * IQR))
+    | (mv.df[mv.numeric_features] > (Q3 + 1.5 * IQR))
+).any(axis=1)
+
+categorical_features = mv.df.select_dtypes(include=["object"]).columns
+df = pd.concat(
+    [mv.df_filtered_numeric, mv.df.loc[condition, categorical_features]], axis=1
+)
